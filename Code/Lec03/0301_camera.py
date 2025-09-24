b7e16a95742bd99d9e3d4db8e268a9d7bef94dab
Dknife <young.min.kang@gmail.com>
2025-09-24 14:20:41 +0900

    Add files via upload

--- Code/Lec03/0301_camera.py
+++ Code/Lec03/0301_camera.py
@@ -2,70 +2,52 @@ import glfw
 from myglfw import *
 from OpenGL.GL import *
 
-
-# Three callback functions for OpenGL Graphics
-
+# 축 그리기
 def drawAxes():
     glBegin(GL_LINES)
-    # x축 (0,0,0) - (1,0,0)
-    glColor3f(1, 0, 0) # 빨간색
-    glVertex3f(0,0,0)
-    glVertex3f(1,0,0)
-    # y축
-    glColor3f(0, 1, 0) # 녹색
-    glVertex3f(0,0,0)
-    glVertex3f(0,1,0)
-    # z축
-    glColor3f(0, 0, 1) # 파란색
-    glVertex3f(0,0,0)
-    glVertex3f(0,0,1)
+    # X
+    glColor3f(1, 0, 0)
+    glVertex3f(0, 0, 0)
+    glVertex3f(1, 0, 0)
+    # Y
+    glColor3f(0, 1, 0)
+    glVertex3f(0, 0, 0)
+    glVertex3f(0, 1, 0)
+    # Z
+    glColor3f(0, 0, 1)
+    glVertex3f(0, 0, 0)
+    glVertex3f(0, 0, 1)
     glEnd()
-    glColor3f(1,1,1) # 흰색으로 복원
+
+def drawTriangle():
+    glColor3f(0, 1, 1)
+    glBegin(GL_TRIANGLES)
+    glVertex3f(-0.5,-0.5, 0)
+    glVertex3f( 0.5,-0.5, 0)
+    glVertex3f( 0.0, 0.5, 0)
+    glEnd()
 
 def initialize(window):
     glClearColor(0.0, 0.0, 0.0, 1.0)
 
 def display(window):
-    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT)
-
-    glMatrixMode(GL_MODELVIEW)
-    glLoadIdentity()
-
-    glColor3f(0, 1, 1)
-    glBegin(GL_TRIANGLES)
-    glVertex3f(-0.5, -0.5, 0)
-    glVertex3f(0.5, -0.5, 0)
-    glVertex3f(0, 0.5, 0)
-    glEnd()
+    glClear(GL_COLOR_BUFFER_BIT)
 
     drawAxes()
+    drawTriangle()
 
+def reshape(window, w, h) :
+    glViewport(0, 0, w, h)
 
-def reshape(window, width, height):
-    glViewport(0, 0, width, height)
-    glMatrixMode(GL_PROJECTION)
-    glLoadIdentity()
-    glOrtho(-2, 2, -2, 2, -1, 1)
-
-    
-##########################################################
 
 def main():
-    window = initialize_window(500, 500, "glfw window")
-    
-    # Set up callbacks similar to GLUT
-    glfw.set_framebuffer_size_callback(window, reshape)
-    
-    # Manually apply initial reshape
-    width, height = glfw.get_framebuffer_size(window)
-    reshape(window, width, height)
-    
-    # Call initialize
-    initialize(window)
-    
-    # Main loop calling display
-    main_loop(window, display)
+    window = initialize_window(500, 500, "my camera test")
 
+    glfw.set_framebuffer_size_callback(window, reshape)
+
+    initialize(window)
+    main_loop(window, display)
 
 if __name__ == "__main__":
     main()
+
