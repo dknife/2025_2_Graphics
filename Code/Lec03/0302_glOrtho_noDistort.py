5e4e2ccc0c541f8c48718f3f022a01cbef891689
Dknife <young.min.kang@gmail.com>
2025-09-24 14:46:23 +0900

    Add files via upload

---
+++ Code/Lec03/0302_glOrtho_noDistort.py
@@ -0,0 +1,61 @@
+import glfw
+from myglfw import *
+from OpenGL.GL import *
+
+# 축 그리기
+def drawAxes():
+    glBegin(GL_LINES)
+    # X
+    glColor3f(1, 0, 0)
+    glVertex3f(0, 0, 0)
+    glVertex3f(1, 0, 0)
+    # Y
+    glColor3f(0, 1, 0)
+    glVertex3f(0, 0, 0)
+    glVertex3f(0, 1, 0)
+    # Z
+    glColor3f(0, 0, 1)
+    glVertex3f(0, 0, 0)
+    glVertex3f(0, 0, 1)
+    glEnd()
+
+def drawTriangle():
+    glColor3f(0, 1, 1)
+    glBegin(GL_TRIANGLES)
+    glVertex3f(-0.5,-0.5, 0)
+    glVertex3f( 0.5,-0.5, 0)
+    glVertex3f( 0.0, 0.5, 0)
+    glEnd()
+
+def initialize(window):
+    glClearColor(0.0, 0.0, 0.0, 1.0)
+
+def display(window):
+    glClear(GL_COLOR_BUFFER_BIT)
+
+    drawAxes()
+    drawTriangle()
+
+def reshape(window, w, h) :
+    glViewport(0, 0, w, h)
+    aspect_ratio = w / h
+
+    l, r, b, t, n, f = -1, 1, -1, 1, -1, 1
+    glMatrixMode(GL_PROJECTION)
+    glLoadIdentity()
+    glOrtho(l*aspect_ratio, r*aspect_ratio, b, t, n, f)
+
+
+def main():
+    window = initialize_window(500, 500, "my camera test")
+
+    glfw.set_framebuffer_size_callback(window, reshape)
+    w, h = glfw.get_framebuffer_size(window)
+    reshape(window, w, h)
+
+    initialize(window)
+    main_loop(window, display)
+
+if __name__ == "__main__":
+    main()
+
