401e3f1b82f9837b016cb72392f6f5504494aefd
Dknife <young.min.kang@gmail.com>
2025-10-29 15:44:36 +0900

    Update robot.py

--- Code/Code07_robot/robot.py
+++ Code/Code07_robot/robot.py
@@ -7,19 +7,30 @@ from OpenGL.GLU import *  # 추가: gluLookAt을 사용하기 위해 GLU 임포�
 import Camera
 import common
 
+t = 0
+
 main_camera = Camera.Camera()
+
+def drawPart(width, height, depth):
+    glPushMatrix()
+    glScalef(width, height, depth)
+    common.drawWireCube()
+    glPopMatrix()
 
 def initialize(window):
     glClearColor(0.6, 0.6, 0.6, 1.0)
     glLineWidth(2)
     glEnable(GL_DEPTH_TEST)
 
-    main_camera.eye = np.array([2,2,6])
+    main_camera.eye = np.array([-5,5,10])
     main_camera.at = np.array([0,0,0])
     main_camera.up = np.array([0,1,0])
 
 def display(window):
-    global angle
+    global t
+
+    t += 0.01
+    angle = 45 * np.sin(t)
 
     glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT)
     
@@ -27,10 +38,41 @@ def display(window):
     common.drawPlane()
     common.drawAxes()
 
-    glColor3f(1, 1, 1)
-    common.drawWireCube()
+    glColor3f(1, 1, 1)    
+    glTranslatef(0, 0.5, 0)
+    drawPart(2, 1, 2) # 몸통
 
+    # 부모 높이의 반만 올라온다.
+    glTranslatef(0, 0.5, 0)
+    glRotatef(angle, 0, 0, 1)
+    # 내 길이의 반 올라간다.
+    glTranslatef(0, 1.5, 0)
+    drawPart(0.5, 3, 0.5) # 팔 1
 
+    # 부모의 높이 반 올라가기
+    glTranslatef(0, 1.5, 0)
+    glRotatef(angle*0.5, 0, 0, 1)
+    glTranslatef(0, 0.5, 0)
+    drawPart(0.5, 1, 0.5) # 팔 2
+
+    glPushMatrix()
+
+    # 손1
+    # 부모의 반
+    glTranslatef(0, 0.5, 0)
+    glRotatef(angle, 0, 0, 1)
+    glTranslatef(0, 0.25, 0)
+    drawPart(0.1, 0.5, 0.5)
+
+    # 팔2의 변환 상태로 돌아간다...
+    glPopMatrix()
+
+    # 손2    
+    # 부모의 반
+    glTranslatef(0, 0.5, 0)
+    glRotatef(-angle, 0, 0, 1)
+    glTranslatef(0, 0.25, 0)
+    drawPart(0.1, 0.5, 0.5)
 
 
 def reshape(window, width, height):
