54f27b45e9913f7801a6d412dffdbd8792912dae
Dknife <young.min.kang@gmail.com>
2025-11-05 16:38:48 +0900

    Add files via upload

--- Code/Code08_Color/03MeshLighting.py
+++ Code/Code08_Color/03MeshLighting.py
@@ -11,17 +11,51 @@ main_camera = Camera.Camera()
 mymesh = Mesh.Mesh()
 angle = 0
 
+
+# 조명 계산에 필요한 데이터를 준비하자
+# 재질: 물체의 특성
+mat_ambient = [0.0, 0.0, 0.0, 0.0] # 주변광에 반응 (ambient material)
+mat_diffuse = [1.0, 1.0, 0.0, 1.0] # 난반사광에 반응 (diffuse material)
+mat_specular= [1.0, 1.0, 1.0, 1.0] # 정반사광에 반응 (specular material)
+mat_shininess = [8] # 반질반질함 (shininess material)
+
+# 광원 설정
+lit_ambient = [0.0, 0.0, 0.0, 1.0] # 물체에 가해질 주변 광
+lit_diffuse = [1.0, 1.0, 1.0, 1.0] # 난반사를 일으킬 빛의 색
+lit_specular =[1.0, 1.0, 1.0, 1.0] # 정반사를 일으킬 빛의 색
+
+lit_position = [1, 1, 1, 0] # 방향광 directional light
+
+### 재질과 광원을 설정하는 작업을 수행하는 함수 구현
+# 광원 설정: glLight
+# 재질 설정: glMaterial
+def LightSet():
+    glMaterialfv(GL_FRONT, GL_AMBIENT, mat_ambient)
+    glMaterialfv(GL_FRONT, GL_DIFFUSE, mat_diffuse)
+    glMaterialfv(GL_FRONT, GL_SPECULAR, mat_specular)
+    glMaterialfv(GL_FRONT, GL_SHININESS, mat_shininess)
+
+    glLightfv(GL_LIGHT0, GL_AMBIENT, lit_ambient)
+    glLightfv(GL_LIGHT0, GL_DIFFUSE, lit_diffuse)
+    glLightfv(GL_LIGHT0, GL_SPECULAR, lit_specular)
+
+def LightPositioning():
+    glLightfv(GL_LIGHT0, GL_POSITION, lit_position)
+
 def initialize(window):
     glClearColor(0.6, 0.6, 0.6, 1.0)
     glEnable(GL_DEPTH_TEST)
 
     mymesh.loadMesh("./cow.txt")
-    #mymesh.prepareDisplayList()
     mymesh.prepareForBufferRendering()
 
     main_camera.eye = np.array([3,4,5])
     main_camera.at = np.array([0,0,0])
     main_camera.up = np.array([0,1,0])
+
+    LightSet()
+    glEnable(GL_LIGHTING)
+    glEnable(GL_LIGHT0)
 
 def display(window):
     global angle
@@ -29,11 +63,19 @@ def display(window):
     glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT)
     
     main_camera.apply() 
+    LightPositioning()
+
+    glMaterialfv(GL_FRONT, GL_DIFFUSE, [1.0, 0.0, 0.0, 1.0])
+    common.drawPlane()
+
+    glDisable(GL_LIGHTING)
     common.drawAxes()
+    glEnable(GL_LIGHTING)
 
     angle += 1
     glRotatef(angle, 0, 1, 0)
-    
+
+    glMaterialfv(GL_FRONT, GL_DIFFUSE, [1.0, 1.0, 0.0, 1.0])
     glColor3f(1, 1, 0)
     mymesh.drawBuffer()
 
--- Code/Code08_Color/Mesh.py
+++ Code/Code08_Color/Mesh.py
@@ -8,14 +8,15 @@ class Mesh():
     def __init__(self):
         self.nV = 0
         self.nF = 0
-        self.vBuffer = None
-        self.iBuffer = None
-        self.listNumber = 0
+        self.normalBuffer = None
+        self.vertexBuffer = None
     
     ##  drawArrays, drawElements 사용하기
     def prepareForBufferRendering(self):
         glEnableClientState(GL_VERTEX_ARRAY)
         glVertexPointer(3, GL_FLOAT, 0, self.vertexBuffer)
+        glEnableClientState(GL_NORMAL_ARRAY)
+        glNormalPointer(GL_FLOAT, 0, self.normalBuffer)
 
     def drawBuffer(self):
         glDrawElements(GL_TRIANGLES, self.nF * 3, GL_UNSIGNED_INT, self.idxBuffer)
@@ -27,6 +28,9 @@ class Mesh():
             self.nV = int(next(inputFile))
             # 정점을 담을 버텍스 배열을 준비한다.
             self.vertexBuffer = np.zeros(shape = ( self.nV * 3, ), dtype = float)
+            # 각 정점마타 법선 정보를 담도록 법선 배열도 준비한다.
+            self.normalBuffer = np.zeros(shape = ( self.nV * 3, ), dtype = float)
+
             # self.nV 정점의 개수만큼 반복적으로 정점 정보를 읽는다.
             for i in range(self.nV):
                 input_string = next(inputFile)
@@ -50,6 +54,28 @@ class Mesh():
                 start = i * 3
                 end = start + 3
                 self.idxBuffer[start: end] = input_string.split()[1:4]
+                
+                ## 면마다 법선을 계산하여 해당 면을 이루는 정점에 법선 정보를 누적
+                # 정점 인덱스
+                index = self.idxBuffer[start: end]
+                # 세 정점의 좌표를 구한다
+                p0 = self.getVertex(index[0])
+                p1 = self.getVertex(index[1])
+                p2 = self.getVertex(index[2])
+                u = p1 - p0
+                v = p2 - p0
+                N = np.cross(u, v)
+                self.normalBuffer[index[0]*3: index[0]*3 + 3] += N
+                self.normalBuffer[index[1]*3: index[1]*3 + 3] += N
+                self.normalBuffer[index[2]*3: index[2]*3 + 3] += N
+
+            for i in range(self.nV):
+                N = self.normalBuffer[i*3: i*3 + 3] # i번째 정점의 법선 누적값
+                norm = np.linalg.norm(N) # 누적된 벡터의 길이를 계산
+                N = N / norm # 정규화
+                self.normalBuffer[i*3: i*3 + 3] = N # 정규화된 법선벡터 다시 저장
+
+
     
     def getVertex(self, idx):
         return self.vertexBuffer[ idx*3 : idx*3 + 3]  # idx*3 위치에서 3개의 점을 가져온다.
